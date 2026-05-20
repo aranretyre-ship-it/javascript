06d20aea7fd0c8051cf6600806d67b893a472a28
aranretyre-ship-it <aranretyre@gmail.com>
2026-05-20 14:23:39 +0300

    Update functions.js

--- lab3/functions.js
+++ lab3/functions.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 /**
  * Возвращает x, возведённое в n-ную степень.
  *
@@ -14,10 +12,6 @@ function pow(x, n) {
   return x * pow(x, n - 1);
 }
 
-// console.log(pow(2, 2));  // 4
-// console.log(pow(2, 0));  // 1
-// console.log(pow(2, -2)); // 0.25
-
 /**
 * Возвращает сумму чисел от 1 до n,
 * используя арифметическую прогрессию
@@ -29,10 +23,6 @@ function sumTo(n) {
   return (n * (n + 1)) / 2;
 }
 
-// console.log(sumTo(100)); // 5050
-
-
-
 /**
  * Проверяет год на високосность
  *
@@ -42,12 +32,6 @@ function sumTo(n) {
 function isLeapYear(year) {
   return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
 }
-
-// console.log(isLeapYear(2024)); //true
-// console.log(isLeapYear(2025)); //false
-// console.log(isLeapYear(2000)); //true
-// console.log(isLeapYear(1900)); //false
-
 
 /**
 * Возвращает факториал числа n.
@@ -61,11 +45,7 @@ function factorial(n) {
   return BigInt(n) * factorial(n - 1);
 }
 
-// console.log(factorial(0)); // 1n
-// console.log(factorial(5)); // 120n
-
-// Функция fib(n), возвращающая n-е число Фибоначчи
-/**
+/**Функция fib(n), возвращающая n-е число Фибоначчи
  * Возвращает n-ое число Фибоначчи.
  *
  * @param {number} n номер искомого числа Фибоначчи
@@ -82,8 +62,8 @@ export function fib(n) {
   return a;
 }
 
-console.log(fib(0));   // 0n
-console.log(fib(100)); // 354224848179261915075n
+console.log(fib(0));  
+console.log(fib(100)); 
 
 /**
  *  * Возвращает результат сравнения целых чисел y и x.
@@ -100,11 +80,6 @@ function compare(x) {
   };
 }
 
-// console.log(compare(5)(4)); // false
-// console.log(compare(5)(5)); // null
-// console.log(compare(5)(6)); // true
-
-
 /**
  * Возвращает сумму всех аргументов.
  *
@@ -114,7 +89,3 @@ function compare(x) {
 function sum(...args) {
   return args.reduce((acc, curr) => acc + curr, 0);
 }
-
-// console.log(sum());       // 0
-// console.log(sum(1));      // 1
-// console.log(sum(1, 2));   // 3
